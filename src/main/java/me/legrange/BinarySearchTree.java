d88dd8e74bc373edb281be51d4a94b8767541afa
Gideon Le Grange <gideon@legrange.me>
2021-07-15 17:57:47 +0200

    Cleaned up code

--- src/main/java/me/legrange/BinarySearchTree.java
+++ src/main/java/me/legrange/BinarySearchTree.java
@@ -1,14 +1,5 @@
 package me.legrange;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public final class BinarySearchTree<T extends Comparable> extends AbstractBinaryTree<T> {
 
     /**
@@ -53,55 +44,13 @@ public final class BinarySearchTree<T extends Comparable> extends AbstractBinary
         return findParent(root, data).getData();
     }
 
-
-    /**
-     * Recursively set up a depth first stream.
+    /** Find the best node for the given data. Best means the value
+     * compares to the data in the node, or there are no children to search for this node.
      *
-     * @param data The point in the tree to work from
-     * @return The stream
+     * @param node
+     * @param value
+     * @return
      */
-    private Stream<T> makeDepthStream(T data) {
-        Optional<T> left = getLeft(data);
-        Optional<T> right = getRight(data);
-        return Stream.concat(Stream.of(data),
-                Stream.concat(
-                        left.isPresent() ? Stream.of(left.get()).flatMap(this::makeDepthStream) : Stream.empty(),
-                        right.isPresent() ? Stream.of(right.get()).flatMap(this::makeDepthStream) : Stream.empty()));
-    }
-
-    /**
-     * Recursively set up a breadth first stream.
-     *
-     * @return The stream
-     */
-    private Stream<T> makeBreadthStream(List<T> data) {
-        if (data.isEmpty()) {
-            return Stream.empty();
-        }
-        return Stream.concat(data.stream(),
-                makeBreadthStream(data.stream().flatMap(object -> streamOfChildren(object)).collect(Collectors.toList())));
-    }
-
-    private Stream<T> streamOfChildren(T val) {
-        Optional<T> left = getLeft(val);
-        Optional<T> right = getRight(val);
-        return Stream.concat(left.isPresent() ? Stream.of(left.get()) : Stream.empty(), right.isPresent() ? Stream.of(right.get()) : Stream.empty());
-    }
-
-    private int calculateDepth(BinaryNode<T> node) {
-        if (node == null) {
-            return 0;
-        }
-        return 1 + Math.max(calculateDepth(node.getLeft()), calculateDepth(node.getRight()));
-    }
-
-    private int calculateWidth(BinaryNode<T> node) {
-        if (node == null) {
-            return 0;
-        }
-        return Math.max(1, calculateWidth(node.getLeft()) + calculateWidth(node.getRight()));
-    }
-
     private BinaryNode<T> findParent(BinaryNode<T> node, T value) {
         int diff = value.compareTo(node.getData());
         if (diff < 0) {
